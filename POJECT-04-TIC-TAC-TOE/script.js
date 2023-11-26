b8005f763f5b4ffc73cc6c5cf58f2a4efd645d6a
Saba Sunawar <sabasunawar@gmail.com>
2023-11-26 09:36:17 -0800

    game completed. Added two more functions. 1. highlight win sequence match win sequence

--- POJECT-04-TIC-TAC-TOE/script.js
+++ POJECT-04-TIC-TAC-TOE/script.js
@@ -64,48 +64,84 @@ const checkWinner = ()=>{
 
     if(matchsequence(0,1,2)){
         winner=currentPlayer;
-        message.innerText=`Player ${currentPlayer} won.`
+        message.innerText=`Player ${currentPlayer} won.`;
+        highlightWin(0,1,2)
         return;
     }
     if(matchsequence(3,4,5)){
         winner=currentPlayer;
-        message.innerText=`Player ${currentPlayer} won.`
+        message.innerText=`Player ${currentPlayer} won.`;
+        highlightWin(3,4,5)
         return;
     }
     if(matchsequence(6,7,8)){
         winner=currentPlayer;
-        message.innerText=`Player ${currentPlayer} won.`
+        message.innerText=`Player ${currentPlayer} won.`;
+        highlightWin(6,7,8)
         return;
     }
     if(matchsequence(0,3,6)){
         winner=currentPlayer;
-        message.innerText=`Player ${currentPlayer} won.`
+        message.innerText=`Player ${currentPlayer} won.`;
+        highlightWin(0,3,6)
         return;
     }
     if(matchsequence(1,4,7)){
         winner=currentPlayer;
-        message.innerText=`Player ${currentPlayer} won.`
+        message.innerText=`Player ${currentPlayer} won.`;
+        highlightWin(1,4,7)
         return;
     }
     if(matchsequence(2,5,8)){
         winner=currentPlayer;
-        message.innerText=`Player ${currentPlayer} won.`
+        message.innerText=`Player ${currentPlayer} won.`;
+        highlightWin(2,5,8)
         return;
     }
     if(matchsequence(0,4,8)){
         winner=currentPlayer;
-        message.innerText=`Player ${currentPlayer} won.`
+        message.innerText=`Player ${currentPlayer} won.`;
+        highlightWin(0,4,8)
         return;
     }
     if(matchsequence(2,4,6)){
         winner=currentPlayer;
-        message.innerText=`Player ${currentPlayer} won.`
+        message.innerText=`Player ${currentPlayer} won.`;
+        highlightWin(2,4,6)
         return;
     }
 }
 
+// 5 - Function to check Game draw condition
 
+const gameDraw = ()=>{
+    let totalnullvalues=0;
+    board.forEach(value =>{ 
+     if(value === null){
+         
+        totalnullvalues ++;
+     }
+    })
+    if(totalnullvalues === 0 && winner === null){
 
+        message.innerText=`Game Draw`;
+        if( !player1.classList.contains('disable')){
+            player1.classList.add('disable');
+        }
+        if(!player2.classList.contains('disable')){
+            player2.classList.add('disable');
+        } 
+    }
+}
+
+// 6 - Function to highlight the win sequence
+
+const highlightWin =(box1,box2,box3) =>{
+
+    document.getElementById(box1).classList.add('win');
+    document.getElementById(box2).classList.add('win');
+    document.getElementById(box3).classList.add('win');
+}
 
 // ALL EVENT LISTENERS
 // 1 - Event listen to listen click on all boxes
@@ -118,7 +154,7 @@ Array.from(allBoxes).forEach((box) =>{
             updateBoard();
             checkWinner();
             togglePlayer();
-            
+            gameDraw();
         };
       
     } );
